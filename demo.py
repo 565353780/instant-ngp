c7cef5677f5eefd31e6c17c79db1adb7fc208ef2
chLi <565353780@qq.com>
2022-09-19 15:50:23 +0800

    simplify demo func

--- demo.py
+++ demo.py
@@ -41,9 +41,47 @@ def showNeRF():
     runInstantNGP(scene_folder_path)
     return True
 
-if __name__ == "__main__":
-    getImages()
+def demo():
+    # videoToImages
+    video_file_path = "/home/chli/chLi/NeRF/chair1/chair1.mp4"
+    save_image_folder_path = "/home/chli/chLi/NeRF/chair1/images/"
+    skip_num = 20
+    scale = 1
+    show_image = False
+    print_progress = True
+
+    # colmap2Nerf
+    image_folder_path = "/home/chli/chLi/NeRF/chair1/images"
+    colmap_camera_text_folder_path = "/home/chli/chLi/NeRF/chair1/sparse"
+    save_transform_json_folder_path = "/home/chli/chLi/NeRF/chair1/transform.json"
+    aabb_scale = 16
+
+    # runInstantNGP
+    scene_folder_path = "/home/chli/chLi/NeRF/chair1"
+
+    videoToImages(
+        video_file_path,
+        save_image_folder_path,
+        skip_num,
+        scale,
+        show_image,
+        print_progress
+    )
+
     finished = input("please run colmap to generate camera pose! then input anything to continue.")
-    getNeRF()
-    showNeRF()
+    if finished == "q":
+        return True
+
+    colmap2Nerf(
+        image_folder_path,
+        colmap_camera_text_folder_path,
+        save_transform_json_folder_path,
+        aabb_scale
+    )
+
+    runInstantNGP(scene_folder_path)
+    return True
+
+if __name__ == "__main__":
+    demo()
 
