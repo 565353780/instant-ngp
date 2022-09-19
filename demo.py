a18551d2a02e498e477576561b566ffab4d76ae3
chLi <565353780@qq.com>
2022-09-19 14:04:50 +0800

    finish videoToImages, test passed

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from instant_ngp_recon.Method.image import createImages
+from instant_ngp_recon.Method.image import videoToImages
 
 if __name__ == "__main__":
-    print("start test")
+    video_file_path = "/home/chli/chLi/NeRF/keyboard.mp4"
+    save_image_folder_path = "/home/chli/chLi/NeRF/keyboard/"
+
+    videoToImages(video_file_path, save_image_folder_path)
 
--- instant_ngp_recon/Method/image.py
+++ instant_ngp_recon/Method/image.py
@@ -1,6 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def createImages(video_path, save_image_folder_path):
+import os
+import cv2
+
+def videoToImages(video_file_path,
+                  save_image_folder_path,
+                  scale=1,
+                  show_image=False):
+    if save_image_folder_path[-1] != "/":
+        save_image_folder_path += "/"
+
+    os.makedirs(save_image_folder_path, exist_ok=True)
+
+    cap = cv2.VideoCapture(video_file_path)
+
+    image_idx = 0
+    skip_num = 20
+
+    while True:
+        status, frame = cap.read()
+        image_idx += 1
+
+        if image_idx % skip_num != 0:
+            continue
+
+        if not status:
+            break
+
+        if scale != 1:
+            frame = cv2.resize(frame,(
+                int(frame.shape[1]/scale),
+                int(frame.shape[0]/scale)
+            ))
+
+        if show_image:
+            cv2.imshow("image", frame)
+            cv2.waitKey(1)
+
+        save_image_file_path = save_image_folder_path + \
+            "image_" + str(image_idx) + ".png"
+        cv2.imwrite(save_image_file_path, frame)
     return True
 
